41b482a1dd917a8e9af9911c62dd9230c6039bda
YaelCervantes <elmer.eyca@gmail.com>
2026-05-04 10:40:28 -0600

    fix: force search_path=public for Neon schema resolution

--- server/db.js
+++ server/db.js
@@ -6,7 +6,8 @@ const useSSL = dbUrl.includes('neon.tech') || dbUrl.includes('sslmode=require')
 
 const pool = new Pool({
   connectionString: dbUrl,
-  ssl: useSSL ? { rejectUnauthorized: false } : false
+  ssl: useSSL ? { rejectUnauthorized: false } : false,
+  options: '--search_path=public'
 })
 
 module.exports = pool
